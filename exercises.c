845dcb1bf7625222fe6c45e5b6971ad0d52393a4
Your Name <you@example.com>
2024-03-13 20:59:53 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -44,6 +44,15 @@ y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+{
+  for (int i = 0; i < size; i++);
+  {
+    if (arr[i] % 2 == 0)
+    {
+      newSize++;
+    }
+  }
+}
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
